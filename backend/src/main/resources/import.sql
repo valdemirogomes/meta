d070e5ccd84d2772524dce0e7c5a163f9f1eec40
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2022-10-24 00:25:51 -0300

    inserindo dados no banco H2

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,1 +1,5 @@
 INSERT INTO tb_sales (seller_name, visited, deals, amount, date) VALUES ('Barry Allen', 173, 93, 10816.0, '2021-07-22');
+INSERT INTO tb_sales (seller_name, visited, deals, amount, date) VALUES ('Maria', 173, 93, 10816.0, '2021-07-23');
+INSERT INTO tb_sales (seller_name, visited, deals, amount, date) VALUES ('Jose', 175, 94, 10816.0, '2021-06-26');
+
+
