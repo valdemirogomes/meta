4c6942581280c4c424b208c853eb250a366901bc
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2022-10-23 21:58:35 -0300

    Implementando uma busca por data, retornando uma Page

--- backend/src/main/java/com/dev/meta/services/SaleService.java
+++ backend/src/main/java/com/dev/meta/services/SaleService.java
@@ -1,8 +1,13 @@
 package com.dev.meta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.dev.meta.entities.Sale;
@@ -15,8 +20,20 @@ public class SaleService {
 	private SaleRepository repository;
 	
 	
-	public List<Sale> findSales() {		
-		return repository.findAll();
+	public Page<Sale> findSales(
+			String minDate, 
+			String maxDate, 
+			Pageable pegeable) {
+		
+		//Pegando data atual	
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+		//Convertendo data minima e data maxima de String para LocalDate
+		LocalDate min = minDate.equals("") ? today.minusMonths(365): LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today: LocalDate.parse(maxDate);
+		
+		
+		return repository.findSales(min, max, pegeable);
 		
 	}
 
